f8608157a17369dc6061dfb81d11d3158f1e191a
quhaichuan <1029155580@qq.com>
2020-03-12 23:14:00 +0800

    Thymleaf

--- spring/springboot-Thymleaf/src/main/java/com/qhc/springboot2/MyController.java
+++ spring/springboot-Thymleaf/src/main/java/com/qhc/springboot2/MyController.java
@@ -1,10 +1,14 @@
 package com.qhc.springboot2;
 
 import com.qhc.springboot2.beans.Pet;
+import com.qhc.springboot2.beans.Product;
 import com.qhc.springboot2.beans.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.*;
+
 @Controller
 public class MyController {
 
@@ -13,8 +17,17 @@ public class MyController {
         map.addAttribute("name","<b>Chian</b>,USA,UK");
         map.addAttribute("age","18");
         Pet pet = new Pet("Tom","哈士奇");
-        User user = new User("quhaichuan","20",pet);
+        User user = new User("","20",pet);
         map.addAttribute("user",user);
+
+        List<Product> slist = new ArrayList<>();
+        Product product1 = new Product("苹果","100");
+        Product product2 = new Product("香蕉","50");
+
+        slist.add(product1);
+        slist.add(product2);
+
+        map.addAttribute("allProducts",slist);
         return "list";
     }
 
---
+++ spring/springboot-Thymleaf/src/main/java/com/qhc/springboot2/beans/Product.java
@@ -0,0 +1,30 @@
+package com.qhc.springboot2.beans;
+
+public class Product {
+    private String name;
+    private String price;
+
+    public Product() {
+    }
+
+    public Product(String name, String price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
